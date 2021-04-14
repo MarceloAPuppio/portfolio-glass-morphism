0a7c31ab7eb279f225a319ed9c06cfeb912fdfc2
marceloapuppio <marcelopuppio@gmail.com>
2021-04-14 00:22:18 -0300

    darkmonde new feature
    Detecta si el esquema está en modo oscuro, y lo ajusta srgún dicha preferencia

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 // Referenciamos elementos del DOM
 const buttonDarkMode = document.getElementById("buttondarkmode");
+//Detectamos si está configurado en modo Oscuro
+let darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+darkMode.addEventListener("change", () => {
+  if (darkMode.matches) {
+    // buttonDarkMode.setAttribute("checked", true);
+    buttonDarkMode.checked = true;
+    document.body.classList.add("darkmode");
+  } else {
+    // buttonDarkMode.removeAttribute("checked");
+    buttonDarkMode.checked = false;
+    document.body.classList.remove("darkmode");
+  }
+});
 
 //añadimos eventos
+if (darkMode.matches) {
+  buttonDarkMode.setAttribute("checked", true);
+  document.body.classList.add("darkmode");
+}
 
-buttonDarkMode.addEventListener("change", () =>
-  document.body.classList.toggle("darkmode")
-);
+buttonDarkMode.addEventListener("change", () => {
+  document.body.classList.toggle("darkmode");
+});
